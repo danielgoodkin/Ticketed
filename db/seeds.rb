1bfb03ac23bf0c84173d8c13aa9125e1b0d57e47
Daniel Goodkin <daniel.goodkin@gmail.com>
2015-10-20 17:48:34 +0100

    Added admin to User model, and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "admin@ticketed.com")
+	User.create!(email: "admin@ticketed.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@ticketed.com")
+	User.create!(email: "viewer@ticketed.com", password: "password", admin: true)
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+	unless Project.exists?(name: name)
+		Project.create!(name: name, description: "A sample project about #{name}")
+	end
+end
